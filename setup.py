5671e21f497c09ac0053310244f9076c31759b37
okesontj <okesontj@users.noreply.github.com>
2016-08-18 09:33:05 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup  # , find_packages
+
+setup(
+    name='APM',
+    version='1.0',
+    description='Python interface for APMonitor',
+    author='John Hedengren',
+    author_email='john_hedengren@byu.edu',
+    py_modules=['apm'],
+    license='Apache License, Version 2.0',
+    zip_safe=False
+)
